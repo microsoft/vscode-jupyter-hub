fbbccd24e594ef0f374ade4843d6522b6d2dd8e4
Don Jayamanne <don.jayamanne@outlook.com>
2023-09-18 15:12:17 +1000

    oops

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -24,7 +24,7 @@ async function main() {
         });
 
         // Download VS Code, unzip it and run the integration test
-        await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: [TEMP_DIR] });
+        await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: [TEMP_DIR], version: 'insiders' });
     } catch (err) {
         console.error('Failed to run tests', err);
         process.exit(1);
